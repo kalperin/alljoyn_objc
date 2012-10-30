15036d02946f74423ac680991aae6122fd57a034
Gregory Arias <c_garias@quicinc.com>
2012-10-30 13:35:44 -0700

    Baseline for FileTransferServer sample on iOS

---
+++ samples/iOS/FileTransferServer/FileTransferServer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FileTransferServer
+//
+//  Created by Guest on 10/30/12.
+//  Copyright (c) 2012 Qualcomm Innovation Center, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ samples/iOS/FileTransferServer/FileTransferServer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FileTransferServer
+//
+//  Created by Guest on 10/30/12.
+//  Copyright (c) 2012 Qualcomm Innovation Center, Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
