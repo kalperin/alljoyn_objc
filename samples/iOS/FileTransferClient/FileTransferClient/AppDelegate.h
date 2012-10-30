a3ed88194d57a943653c995c6d6ebfedbd091d19
Gregory Arias <c_garias@quicinc.com>
2012-10-30 13:36:11 -0700

    Baseline for File Transfer client sample on iOS

---
+++ samples/iOS/FileTransferClient/FileTransferClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FileTransferClient
+//
+//  Created by Guest on 10/30/12.
+//  Copyright (c) 2012 Qualcomm Innovation Center, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ samples/iOS/FileTransferClient/FileTransferClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FileTransferClient
+//
+//  Created by Guest on 10/30/12.
+//  Copyright (c) 2012 Qualcomm Innovation Center, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
