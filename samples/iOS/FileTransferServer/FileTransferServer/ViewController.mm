b6135b42138f10a4469d4b79b6fa497d1abefcde
Gregory Arias <c_garias@quicinc.com>
2012-11-16 14:20:37 -0800

    Completed File Transfer sample for iOS (iPhone/iPad)

--- samples/iOS/FileTransferServer/FileTransferServer/ViewController.mm
+++ samples/iOS/FileTransferServer/FileTransferServer/ViewController.mm
@@ -15,9 +15,15 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #import "ViewController.h"
-#import "Constants.h"
+#import "AJNStatus.h"
+#import "AJNMessageArgument.h"
 #import "AJNServiceController.h"
+#import "Constants.h"
 #import "FileTransferObject.h"
+#import <alljoyn/Message.h>
+
+#define kMaximumAllJoynArrayLength 131072
+NSString * const kImageFileName = @"image";
 
 @interface ViewController () <AJNServiceDelegate>
 
@@ -26,6 +32,8 @@
 
 - (AJNBusObject *)objectOnBus:(AJNBusAttachment *)bus;
 - (void)shouldUnloadObjectOnBus:(AJNBusAttachment *)bus;
+
+- (void)transferFile;
 
 @end
 
@@ -81,8 +89,59 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)transferFile
+{
+    dispatch_queue_t fileTransferQueue = dispatch_queue_create("org.alljoyn.samples.fileTransferQueue", NULL);
+    
+    dispatch_async(fileTransferQueue, ^{
+        NSString *filePath = [[NSBundle mainBundle] pathForResource:@"image" ofType:@"png"];
+        NSFileHandle *fileHandle = [NSFileHandle fileHandleForReadingAtPath:filePath];
+        NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:filePath error:nil];
+        unsigned long long totalFileSize = [fileAttributes fileSize];
+        
+
+        const uint8_t flags = 0x20;
+        
+        NSInteger bytesRead = 0;
+        NSInteger totalBytesRead = 0;
+        NSInteger countOfChunksSent = 0;
+        NSData *fileData;
+        uint8_t buffer[kMaximumAllJoynArrayLength];
+        ajn::MsgArg msgArg;
+    
+        do {
+            fileData = [fileHandle readDataOfLength:kMaximumAllJoynArrayLength];
+            bytesRead = fileData.length;
+            [fileData getBytes:buffer length:kMaximumAllJoynArrayLength];
+            totalBytesRead += bytesRead;
+            countOfChunksSent++;
+            
+            msgArg.Set("ay", bytesRead, buffer);
+
+            AJNMessageArgument *messageArgument = [[AJNMessageArgument alloc] init];
+            [messageArgument setValue:@"ay", bytesRead, buffer];
+            [self.fileTransferObject sendTransferFileNamed:filePath currentIndex:[NSNumber numberWithInteger:countOfChunksSent] fileData:[[AJNMessageArgument alloc] initWithHandle:&msgArg] inSession:self.serviceController.sessionId toDestination:nil flags:flags];
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.progressView.progress = (float)totalBytesRead / (float)totalFileSize;
+            });
+        }
+        while (totalBytesRead < totalFileSize);
+        
+        if (bytesRead != 0) {
+            msgArg.Set("ay", 0, NULL);
+            [self.fileTransferObject sendTransferFileNamed:filePath currentIndex:[NSNumber numberWithInteger:countOfChunksSent] fileData:[[AJNMessageArgument alloc] initWithHandle:&msgArg] inSession:self.serviceController.sessionId toDestination:nil flags:flags];
+            
+        }
+        
+    });
+    
+}
+
 - (IBAction)didTouchStartButton:(id)sender
 {
+    self.eventsTextView.text = nil;
+    
     if (self.serviceController.bus.isStarted) {
         [self.serviceController stop];
         [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
@@ -105,6 +164,14 @@
     self.fileTransferObject = [[FileTransferObject alloc] initWithBusAttachment:self.serviceController.bus onPath:kServicePath];
     
     return self.fileTransferObject;
+}
+
+- (void)didJoin:(NSString *)joiner inSessionWithId:(AJNSessionId)sessionId onSessionPort:(AJNSessionPort)sessionPort
+{
+    [self.serviceController.bus enableConcurrentCallbacks];
+    // start the file transfer once the client joins the session
+    //
+    [self transferFile];
 }
 
 - (void)didReceiveStatusMessage:(NSString*)message
